ba40c79e23ce6ef3bd63ee8a494fb48679fe0de9
RupinSamria <rupin.samria@gmail.com>
2020-02-02 14:34:29 +0530

    Views

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FaCode } from 'react-icon/fa';
+
+function LandingPage() {
+  return (
+    <>
+    <div className='app'>
+      <FaCode style = {{ fontSize: '4em' }}/><br/>
+      <span style={{ fontSize: '2em' }}>Cooooool!!!</span>
+    </div>
+    <div style = {{ float:'right' }}>Tesst</div>
+    </>
+  )
+}
+
+export default LandingPage
